0997609d6d8fd0d15fcb62e2014bcc3fd727dbcc
Josua <josuahel2920@gmail.com>
2024-11-15 17:45:31 -0600

    Creacion del proyecto para la pagina CdO

---
+++ src/main/java/com/c_d_o/proyecto_cdo/ProyectoCdoApplication.java
@@ -0,0 +1,13 @@
+package com.c_d_o.proyecto_cdo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoCdoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoCdoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/c_d_o/proyecto_cdo/ProyectoCdoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c_d_o.proyecto_cdo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoCdoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
